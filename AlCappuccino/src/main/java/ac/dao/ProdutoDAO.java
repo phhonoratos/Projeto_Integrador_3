256ec6c6a3a8bd4752d7f4c3fb0744508e85bdcd
danil <danil@LAPTOP-V9HB8EA1>
2020-10-16 17:29:13 -0300

    Danilo - criação do método para retornar um produto específico.

--- AlCappuccino/src/main/java/ac/dao/ProdutoDAO.java
+++ AlCappuccino/src/main/java/ac/dao/ProdutoDAO.java
@@ -64,7 +64,7 @@ public class ProdutoDAO {
         
         public static void updateProduto(Produto produto) throws ClassNotFoundException, SQLException {
         Connection con = ConexaoDB.getConexao();
-        String query = "update cliente set id=?, tipo=?, preco=?, porcentagem=?, valor_venda=? where nome=?";
+        String query = "update produto set id=?, tipo=?, preco=?, porcentagem=?, valor_venda=? where nome=?";
         PreparedStatement ps = con.prepareStatement(query);
         ps.setInt(1, produto.getId());
         ps.setString(2, produto.getTipo());
@@ -77,9 +77,35 @@ public class ProdutoDAO {
         
         public static void deleteProduto(String nome) throws ClassNotFoundException, SQLException {
         Connection con = ConexaoDB.getConexao();
-        String query = "delete from cliente where nome=?";
+        String query = "delete from produto where nome=?";
         PreparedStatement ps = con.prepareStatement(query);
         ps.setString(1, nome);
         ps.execute();
+    }
+        
+        public static Produto getProduto(String nome) {
+        Produto produto = null;
+        try {
+            Connection con = ConexaoDB.getConexao();
+            String query = "select * from produto where nome=?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, nome);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("id");
+                 String tipo = rs.getString("tipo");
+                 double preco = rs.getDouble("preco");  
+                 double porcentagem = rs.getDouble("porcentagem");
+                 double valor_venda = rs.getDouble("valor_venda");
+                 produto = new Produto(id, tipo, nome, preco, porcentagem, valor_venda);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ServletBD.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ServletBD.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return produto;
     }
 }
