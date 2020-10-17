162b95390ffe4a0193d7b47ef830c6f91d2d10d8
roschel <joaomroschel@gmail.com>
2020-10-16 23:06:08 -0300

    adding list cliente servlet

---
+++ AlCappuccino/src/main/java/ac/servlet/ListarCliente.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.servlet;
+
+import ac.dao.ClienteDAO;
+import ac.entidade.Cliente;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author joao
+ */
+public class ListarCliente extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<Cliente> listaClientes = ClienteDAO.getClientes();
+        request.setAttribute("listaClientes", listaClientes);
+
+        RequestDispatcher requestDispatcher = getServletContext()
+                .getRequestDispatcher("/listaClientes.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
