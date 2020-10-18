71acc6eb381c3e92dc79fda12f2ae1aec5a2fcda
Paulo Honorato <paulohonoratos@hotmail.com>
2020-10-18 15:47:41 -0200

    Criação do servlet Listar Funcionarios

---
+++ AlCappuccino/src/main/java/ac/servlet/ListarFuncionarios.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.servlet;
+
+import ac.dao.FuncionariosDAO;
+import ac.entidade.Funcionarios;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author paulo
+ */
+public class ListarFuncionarios extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Funcionarios> funcionarios = FuncionariosDAO.getFuncionarios();
+        
+        request.setAttribute("listaFuncionarios", funcionarios);
+        
+        RequestDispatcher requestDispatcher = 
+                getServletContext().getRequestDispatcher("/listarFuncionarios.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+}
