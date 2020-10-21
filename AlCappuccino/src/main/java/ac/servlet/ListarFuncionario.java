8de3e6ef0fc3fc3bc7259c172195f0bff0acbfb4
Paulo Honorato <paulohonoratos@hotmail.com>
2020-10-21 21:32:36 -0200

    Criando o servlet para listar apenas um funcionário via cpf

---
+++ AlCappuccino/src/main/java/ac/servlet/ListarFuncionario.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.servlet;
+
+import ac.dao.FuncionariosDAO;
+import ac.entidade.Funcionarios;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author paulo
+ */
+public class ListarFuncionario extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String cpf = request.getParameter("cpf");
+        Funcionarios funcionario = FuncionariosDAO.getFuncionario(cpf);
+        
+        request.setAttribute("funcionario", funcionario);
+        
+        RequestDispatcher requestDispatcher = 
+                getServletContext().getRequestDispatcher("/listarFuncionario.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+}
