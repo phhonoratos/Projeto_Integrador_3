e9bb4972f9a369de88f5da4ec948e93579fccaa3
roschel <joaomroschel@gmail.com>
2020-10-21 19:52:02 -0300

    excluindo cliente :heavy_check_mark:

---
+++ AlCappuccino/src/main/java/ac/servlet/ExcluirCliente.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.servlet;
+
+import ac.dao.ClienteDAO;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author joao
+ */
+public class ExcluirCliente extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String cpf = request.getParameter("cpf");
+        try {
+            ClienteDAO.deleteCliente(cpf);
+            response.getWriter().print(true);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ExcluirCliente.class.getName()).log(Level.SEVERE, null, ex);
+            response.getWriter().print(false);
+        }
+    }
+
+}
