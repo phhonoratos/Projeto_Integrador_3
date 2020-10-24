fe27cc0c69b5b033223f5a57da98de6cbf8aacb3
sillasHead <sillas.ap16@gmail.com>
2020-10-24 16:23:48 -0200

    Criando servlet para listar vendas do relatorio

---
+++ AlCappuccino/src/main/java/ac/servlet/ListarVenda.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.servlet;
+
+import ac.dao.VendaDAO;
+import ac.entidade.Venda;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sillas.clpinto
+ */
+public class ListarVenda extends HttpServlet {
+@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        try {
+//            List<Venda> vendas = VendaDAO.select();
+//            
+//            request.setAttribute("vendas", vendas);
+//            
+//            RequestDispatcher rd = getServletContext().getRequestDispatcher("/relatorio/relatorio.jsp");
+//            rd.forward(request, response);
+//        } catch (ClassNotFoundException | SQLException ex) {
+//            Logger.getLogger(ListarVenda.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+}
