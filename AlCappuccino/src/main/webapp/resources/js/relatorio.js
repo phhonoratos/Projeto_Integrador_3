d38dab909c70ae451dbb23363429077b11c5a214
sillasHead <sillas.ap16@gmail.com>
2020-10-26 15:05:09 -0300

    ajustando tela

--- AlCappuccino/src/main/webapp/resources/js/relatorio.js
+++ AlCappuccino/src/main/webapp/resources/js/relatorio.js
@@ -1,8 +1,13 @@
-//function listarVendas() {
-//    var dataInicial = $('#');
-//    var url = 'ListarVenda?dataInicial='+dataInicial+'&dataFinal='+dataFinal;
-//    $.get(url, function (data) {
-//        console.log(data);
-//        location.reload();
-//    });
-//}
+function teste() {
+    alert('qualquer coisa');
+//    var object = document.createElement("button");
+//    document.getElementById('teste').appendChild(object);
+}
+
+function listarDetalhes(vendas) {
+    var object = JSON.parse(vendas);
+    console.log(object);
+//    $("#excluirCliente").modal("show");
+//    $("#nome").html(nome);
+//    $("#cpf").html(cpf);
+}
